79ab38e87b1faca26c2571b840ccce46c6862c26
Freeman72rus <62168110+Freeman72rus@users.noreply.github.com>
2020-03-20 00:07:23 +0500

    2020.03.20 00:07

--- src/InventoryFrame.java
+++ src/InventoryFrame.java
@@ -13,11 +13,21 @@ class InventoryFrame extends JFrame implements MouseListener, ActionListener, Ch
     YurbassFishing yf = new YurbassFishing();
     JButton backButton;
     JButton differButton;
+    JButton lineListButton;
     JLabel spinImage;
+    JLabel lineInfo;
+    JLabel hookImage;
+    JLabel spoonImage;
+    JLabel baitsImage;
+    JLabel katImage;
     JLabel info;
     JComboBox spinCB;
+    JList lineList;
+    JScrollPane lineScroll;
+    JTabbedPane jtab;
     JPanel spinPanel;
     String[] spinInventList;
+    String[] lineInventList;
     Tackle[] tacklesList = new Tackle[100];
 
 
@@ -44,24 +54,58 @@ class InventoryFrame extends JFrame implements MouseListener, ActionListener, Ch
         differButton.setActionCommand("разное");
         differButton.setBounds(685, 492, 85,30);
         spinImage = new JLabel("");
+        lineInfo = new JLabel("");
+        hookImage = new JLabel("");
+        spoonImage = new JLabel("");
+        baitsImage = new JLabel("");
+        katImage = new JLabel("");
+        lineListButton = new JButton("");
+        buttonSetting(lineListButton);
+        lineListButton.setBounds(675,160,110,75);
+        lineListButton.setActionCommand("line");
         //создание выпадающего списка спиннингов
-        createSpinList();
+        createAllTangleList();
+        jtab = new JTabbedPane(SwingConstants.BOTTOM, JTabbedPane.SCROLL_TAB_LAYOUT);
+        jtab.add("", lineScroll);
+        jtab.setLayout(null);
+        jtab.setBounds(430,10,230,180);
         spinCB.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 int spinIndex = spinCB.getSelectedIndex();
                 tacklesList[spinIndex] = new Tackle(UserList.users[YurbassFishing.userSelect].inventory.spinningsUser[spinIndex]);
                 spinImage.setIcon(new ImageIcon(UserList.users[YurbassFishing.userSelect].inventory.spinningsUser[spinIndex].spinPathImage));
-                System.out.println(UserList.users[YurbassFishing.userSelect].inventory.spinningsUser[spinIndex].spinPathImage);
                 spinImage.setBounds(100,150,200,200);
+                if (tacklesList[spinIndex].lineT != null){
+                    lineInfo.setText("<html>Леска<br>" + tacklesList[spinIndex].lineT.lineCapacity + " кг<br>" + "Длина " + tacklesList[spinIndex].lineT.lineLength + " м");
+                    lineInfo.setBounds(200,100,100,100);
+                }
+                if (tacklesList[spinIndex].hookT != null){
+                    hookImage.setIcon(new ImageIcon(tacklesList[spinIndex].hookT.hookPathImage));
+                    hookImage.setBounds(200,200,100,100);
+                }
+                /*if (tacklesList[spinIndex].spoonT != null){
+                    spoonImage.setIcon(new ImageIcon(tacklesList[spinIndex].spoonT.spoonPathImage));
+                    spoonImage.setBounds(200,300,100,100);
+                }*/
+                if (tacklesList[spinIndex].baitsT != null){
+                    baitsImage.setIcon(new ImageIcon(tacklesList[spinIndex].baitsT.baitsPathImage));
+                    baitsImage.setBounds(200,400,100,100);
+                }
+                if (tacklesList[spinIndex].katushkaT != null){
+                    katImage.setIcon(new ImageIcon(tacklesList[spinIndex].katushkaT.katPathImage));
+                    katImage.setBounds(200,500,100,100);
+                }
             }
         });
 
 
         panel.add(backButton);
         panel.add(differButton);
+        panel.add(lineListButton);
         panel.add(spinCB);
         panel.add(spinImage);
+        panel.add(jtab);
 
 
 
@@ -77,6 +121,11 @@ class InventoryFrame extends JFrame implements MouseListener, ActionListener, Ch
             yf.menu.setVisible(true);
             inventFrame.setVisible(false);
             inventFrame.dispose();
+        }
+        if (ae.getActionCommand().equals("line")){
+            jtab.setSelectedIndex(0);
+            //lineScroll.setSize(200,200);
+            //lineScroll.setBounds(200,200,200,200);
         }
     }
 
@@ -126,7 +175,7 @@ class InventoryFrame extends JFrame implements MouseListener, ActionListener, Ch
         button.addChangeListener(this);
         button.addMouseMotionListener(this);
     }
-    void createSpinList(){
+    void createAllTangleList(){
         spinPanel = new JPanel();
         spinInventList = new String[100];
         try{
@@ -150,6 +199,37 @@ class InventoryFrame extends JFrame implements MouseListener, ActionListener, Ch
             System.out.println(e.getMessage());;
         }
         spinCB.setBounds(45,16,230,20);
+
+        int j = 1;
+        try{
+            for (int i = 0; i < UserList.users[YurbassFishing.userSelect].inventory.linesUser.length; i++){
+                if (UserList.users[YurbassFishing.userSelect].inventory.linesUser[i].lineName.contains("кг")){
+                    j++;
+                }
+            }
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println(j);
+        lineInventList = new String[j];
+        lineList = new JList(lineInventList);
+        lineList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        try{
+            int k = 0;
+            for (int i = 0; i < UserList.users[YurbassFishing.userSelect].inventory.linesUser.length; i++){
+                if (UserList.users[YurbassFishing.userSelect].inventory.linesUser[i].lineName.contains("кг")){
+                        lineInventList[k] = UserList.users[YurbassFishing.userSelect].inventory.linesUser[i].lineName;
+                        k++;
+                }
+            }
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        lineScroll = new JScrollPane(lineList);
+        lineScroll.setBounds(0,0,230,180);
+
     }
     public static void main(String[] args){
         SwingUtilities.invokeLater(new Runnable() {
@@ -166,8 +246,6 @@ class InventoryPanel extends JPanel{
         try {
             imageBackground = ImageIO.read(new File("src\\Image\\interface\\invent.jpg"));
             g.drawImage(imageBackground, 0,0, null);
-            //g.setFont(new Font("TimesRoman", Font.PLAIN, 18));
-            //g.drawString("назад", 680, 550);
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/LineList.java
+++ src/LineList.java
@@ -1,8 +1,8 @@
 class LineList {
     static String linePathImage = "src\\\\Image\\\\dev\\\\leska.jpg";
 
-static Line line_1 = new Line("2 кг", linePathImage, 2000, 80, 50);
-static  Line line_2 = new Line("3 кг", linePathImage, 3000, 80, 70);
+static Line line_1 = new Line("2 кг", linePathImage, 2000, 80, 50, -1);
+static  Line line_2 = new Line("3 кг", linePathImage, 3000, 80, 70, -1);
 
 
 static Line[] lineList = {line_1, line_2};
@@ -12,12 +12,14 @@ static Line[] lineList = {line_1, line_2};
         double lineCapacity;
         int lineLength;
         int linePrice;
-        Line(String lineName, String linePathImage, double lineCapacity, int lineLength, int linePrice){
+        int tackleNumber;
+        Line(String lineName, String linePathImage, double lineCapacity, int lineLength, int linePrice, int tackleNumber){
             this.lineName = lineName;
             this.linePathImage = linePathImage;
             this.lineCapacity = lineCapacity;
             this.lineLength = lineLength;
             this.linePrice = linePrice;
+            this.tackleNumber = tackleNumber;
         }
     }
 }
